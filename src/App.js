10bfbdfae82dc0f591ebb78a0f01aa25f223d004
douglas <douglasklemes0303@gmail.com>
2019-10-30 17:00:05 -0300

    Adicionar navbar em home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import NavBar from './navBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-     
+     <NavBar />
     </div>
   );
 }
