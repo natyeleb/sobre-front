b21c1b741d6ed6969f33716aba15cf5f36817ee4
douglas <douglasklemes0303@gmail.com>
2019-10-30 17:02:21 -0300

    Adicionar home com footer e uma imagem

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import NavBar from './navBar/NavBar';
+import Home from './home/Home';
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
+      <Home />
     </div>
   );
 }
