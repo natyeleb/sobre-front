1e1d7854b99819894026347dfe62e287a85dbb70
natyele <natyelebertolanii@gmail.com>
2019-11-01 17:56:35 -0300

    adicionar o texto do sobre

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ListaC from './componentes/lista_de_cursos/Lista_C.js'
 import Curso from './componentes/cadastro/Cursos';
 import Cadastro from './componentes/entreterimentos/Cadastro';
 import Lista_E from './componentes/lista_de_entre/Lista_E';
+import Sobre from './componentes/Sobre/Sobre';
 
 
 
@@ -29,6 +30,7 @@ const App = () => {
       <Route path="/entretenimentos" component={Lista_E}/>
       <Route path="/cadastrocursos" component={Curso}/>
       <Route path="/cadastroentre" component={Cadastro}/>
+      <Route path="/sobre" component={Sobre}/>
 
       </Router>
     </div>
--- src/componentes/Sobre/Sobre.js
+++ src/componentes/Sobre/Sobre.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './Sobre.scss';
+import karen from '../imagem/karen.jpeg';
+import Thiago from '../imagem/thiagoooo.jpeg';
 
 const Sobre = () => {
     return(
-        <div className="">
+        <div className="Tudo">
             <h1>Sobre o nosso projeto!</h1>
             <p>Na cidade de Caieiras a população obtinha em certo nível de  dificuldades para realizar inscrições nas atividades oferecidos pela Prefeitura Municipal de Caieiras ,para isso criamos a plataforma digital DESCOMPLICA.<p/>
             No DESCOMPLICA o usuário consegue realizar a sua inscrição nos cursos disponíveis virtualmente. 
             </p>
-        </div>
 
+        <section className="icones">
+            <span img={karen} alt={karen}></span>
+            
+            <span img={Thiago} alt={Thiago}></span>
+        </section>
+
+        </div>
     );
 }
 
