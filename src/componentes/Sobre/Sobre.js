ce05aefa846267413a1295ff1d3b534e503deee8
natyele <natyelebertolanii@gmail.com>
2019-11-04 16:02:34 -0300

    adicionar os icones da sobre

--- src/componentes/Sobre/Sobre.js
+++ src/componentes/Sobre/Sobre.js
@@ -2,23 +2,26 @@ import React from 'react';
 import './Sobre.scss';
 import karen from '../imagem/karen.jpeg';
 import Thiago from '../imagem/thiagoooo.jpeg';
-import Julio from '../imagem/julio.jpeg';
+import Julio from '../imagem/julio3.jpeg';
 import Douglas from '../imagem/Douglas.jpeg';
+import Natyele from '../imagem/Natyele.jpeg';
 
 const Sobre = () => {
     return(
         <div className="Tudo">
             <h1>Sobre o nosso projeto!</h1>
-            <p>Na cidade de Caieiras a população obtinha em certo nível de  dificuldades para realizar inscrições nas atividades oferecidos pela Prefeitura Municipal de Caieiras ,para isso criamos a plataforma digital DESCOMPLICA.<p/>
-            No DESCOMPLICA o usuário consegue realizar a sua inscrição nos cursos disponíveis virtualmente. 
+            <p>Na cidade de Caieiras a população obtinha em certo nível de  dificuldades para realizar inscrições nas atividades oferecidos pela Prefeitura Municipal de Caieiras ,para isso criamos a plataforma digital <span className="Negrito">DESCOMPLICA</span>.<p/>
+            No <span className="Negrito">DESCOMPLICA</span> o usuário consegue realizar a sua inscrição nos cursos disponíveis virtualmente. 
             </p>
 
+            <h2>DESENVOLVEDORES:</h2>
+        
         <div className="Icones">
-           <span> <img src={karen} alt={karen}/></span>
-           <span> <img src={Thiago} alt={Thiago}/></span>
-           <span> <img src={Julio} alt={Julio}/></span>
-           <span> <img src={Douglas} alt={Douglas}/></span>
-           <span> <img src={karen} alt={karen}/></span>
+           <span> <img src={karen} alt={karen}/>Karen Nunes</span>
+           <span> <img src={Thiago} alt={Thiago}/>Thiago Mendes</span>
+           <span> <img src={Julio} alt={Julio}/>Julio Cesar</span>
+           <span> <img src={Douglas} alt={Douglas}/>Douglas Klemes</span>
+           <span> <img src={Natyele} alt={Natyele}/>Natyele Bertolani</span>
 
         </div>
 
