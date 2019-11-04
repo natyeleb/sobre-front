573e5e222282cb91b02f67b08094b1d82323a577
Karen Nunes <kqueiroz812@gmail.com>
2019-11-04 16:02:38 -0200

    Adicionar elementos a pagina de finalizacao

---
+++ src/componentes/finalE/FinalE.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import  '../finalE/FinalE.scss';
+
+
+
+
+const FinalE= () => {
+    return(
+        <div className='texto'>
+            <h1>Parabéns!</h1>
+            <div className="container">
+                <p></p>
+
+                <p>Numero de inscricap</p>
+                <p>ggvetdyegwdne3y</p>
+
+            </div>
+            
+        </div>
+
+
+    )
+}
+export default FinalE;
+
+
